f5a3795702a75510a3e13170be2e5365d65ff068
gabrielv <vallereaugabriel@gmail.com>
2023-09-05 19:11:01 +0200

    :heavy_plus_sign: Add glpi-singlesignon plugin files

---
+++ plugins/singlesignon/glpi-singlesignon-1.3.4/README.md
@@ -0,0 +1,56 @@
+# Single Sign-On for GLPI
+
+![Lint](https://github.com/edgardmessias/glpi-singlesignon/workflows/Lint/badge.svg)
+[![CodeFactor](https://www.codefactor.io/repository/github/edgardmessias/glpi-singlesignon/badge)](https://www.codefactor.io/repository/github/edgardmessias/glpi-singlesignon)
+[![Total Downloads](https://img.shields.io/github/downloads/edgardmessias/glpi-singlesignon/total.svg)](https://github.com/edgardmessias/glpi-singlesignon/releases)
+[![Current Release](https://img.shields.io/github/release/edgardmessias/glpi-singlesignon.svg)](https://github.com/edgardmessias/glpi-singlesignon/releases/latest)
+
+Single sign-on (SSO) is a property of access control of multiple related, yet independent, software systems. With this property, a user logs in with a single ID and password to gain access to any of several related systems.
+
+For the generic provider, the user is added directly to the database, if it does not exist.
+
+# Installation
+ * Uncompress the archive to the `<GLPI_ROOT>/plugins/singlesignon` directory
+ * Navigate to the Configuration > Plugins page,
+ * Install and activate the plugin.
+
+# Usage
+ * Go to `Configuration > Single Sign-On` and add a provider
+ * To test you can use the test button or logout and try to login with the links below the login pageLogin with `Login with <name>`
+   * It is possible that the test fails with the test button but that does not mean that the configuration is bad. Disconnect and try directly.
+
+# Available providers
+ * Azure (Tenant common) - https://docs.microsoft.com/azure/app-service/configure-authentication-provider-aad
+ * Facebook - https://developers.facebook.com/docs/apps/
+ * GitHub - https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
+ * Google - https://developers.google.com/identity/protocols/OpenIDConnect
+ * Instagram - https://www.instagram.com/developer/authentication/
+ * LinkedIn - https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?context=linkedin/context
+ * Generic - Allow to define custom URLs, ex: With an Azure tenant
+
+# Screenshots
+
+![image 1](./screenshots/image_1.png)
+![image 2](./screenshots/image_2.png)
+![image 3](./screenshots/image_3.png)
+![image 4](./screenshots/image_4.png)
+![image 5](./screenshots/image_5.png)
+![image 6](./screenshots/image_6.png)
+
+# Donation
+<table border="0">
+ <tr>
+    <td align="center">
+    PayPal <br>
+       <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://www.paypal.com/donate?hosted_button_id=5KHYY5ZDTNDSY"> <br>
+       <a href="https://www.paypal.com/donate?hosted_button_id=5KHYY5ZDTNDSY">
+          <img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif">
+       </a>
+    </td>
+    <td align="center">
+       Pix (Brazil) <br>
+       <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=00020126680014BR.GOV.BCB.PIX013628571c52-8b9b-416c-a18f-8e52460608810206Doa%C3%A7%C3%A3o5204000053039865802BR5923Edgard%20Lorraine%20Messias6009SAO%20PAULO61080540900062160512NU50UnEaVM0H63042A45"> <br>
+       28571c52-8b9b-416c-a18f-8e5246060881
+    </td>
+ </tr>
+</table>
---
+++ plugins/singlesignon/glpi-singlesignon-1.3.4/front/callback.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ * SingleSignOn is a plugin which allows to use SSO for auth
+ * ---------------------------------------------------------------------
+ * Copyright (C) 2022 Edgard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ * @copyright Copyright © 2021 - 2022 Edgard
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @link      https://github.com/edgardmessias/glpi-singlesignon/
+ * ---------------------------------------------------------------------
+ */
+
+//Disable CSRF token
+define('GLPI_USE_CSRF_CHECK', 0);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include('../../../inc/includes.php');
+
+$provider_id = PluginSinglesignonToolbox::getCallbackParameters('provider');
+
+if (!$provider_id) {
+   Html::displayErrorAndDie(__sso("Provider not defined."), false);
+}
+
+$signon_provider = new PluginSinglesignonProvider();
+
+if (!$signon_provider->getFromDB($provider_id)) {
+   Html::displayErrorAndDie(__sso("Provider not found."), true);
+}
+
+if (!$signon_provider->fields['is_active']) {
+   Html::displayErrorAndDie(__sso("Provider not active."), true);
+}
+
+$signon_provider->checkAuthorization();
+
+$test = PluginSinglesignonToolbox::getCallbackParameters('test');
+
+if ($test) {
+   $signon_provider->debug = true;
+   Html::nullHeader("Login", $CFG_GLPI["root_doc"] . '/index.php');
+   echo '<div class="left spaced">';
+   echo '<pre>';
+   echo "### BEGIN ###\n";
+   $signon_provider->getResourceOwner();
+   echo "### END ###";
+   echo '</pre>';
+   Html::nullFooter();
+   exit();
+}
+
+$user_id = Session::getLoginUserID();
+
+$REDIRECT = "";
+
+if ($user_id || $signon_provider->login()) {
+
+   $user_id = $user_id ?: Session::getLoginUserID();
+
+   if ($user_id) {
+      $signon_provider->linkUser($user_id);
+   }
+
+   $params = PluginSinglesignonToolbox::getCallbackParameters('q');
+
+   if (isset($params['redirect'])) {
+      $REDIRECT = '?redirect=' . $params['redirect'];
+   } else if (isset($_GET['state']) && is_integer(strpos($_GET['state'], "&redirect="))) {
+      $REDIRECT = '?' . substr($_GET['state'], strpos($_GET['state'], "&redirect=") + 1);
+   }
+
+   Auth::redirectIfAuthenticated();
+
+}
+
+// we have done at least a good login? No, we exit.
+Html::nullHeader("Login", $CFG_GLPI["root_doc"] . '/index.php');
+echo '<div class="center b">' . __('User not authorized to connect in GLPI') . '<br><br>';
+// Logout whit noAUto to manage auto_login with errors
+echo '<a href="' . $CFG_GLPI["root_doc"] . '/front/logout.php?noAUTO=1' .
+str_replace("?", "&", $REDIRECT) . '" class="singlesignon">' . __('Log in again') . '</a></div>';
+echo '<script type="text/javascript">
+   if (window.opener) {
+      $(".singlesignon").on("click", function (e) {
+         e.preventDefault();
+         window.opener.location = $(this).attr("href");
+         window.focus();
+         window.close();
+      });
+   }
+</script>';
+Html::nullFooter();
+exit();
