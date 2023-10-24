91e2e12ae783fdb060fe98edcf786ff3878d279a
gabrielv <vallereaugabriel@gmail.com>
2023-10-24 15:23:55 +0200

    :construction_worker: Manually create db config file from environment

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create database crpyting key from environment
 echo "$SECRET_FILE" | base64 -d > ./config/glpicrypt.key
+
+# Create database config file from environment
+php create_db_config.php
+
 # Start default script for PHP apps
 "$HOME"/bin/run
