20997e675705280f3b72dd0fe355cfe9f83592a3
gabrielv <vallereaugabriel@gmail.com>
2023-10-24 18:07:33 +0200

    :bug: Add missing colon in create_db_config.php

--- bin/create_db_config.php
+++ bin/create_db_config.php
@@ -5,7 +5,7 @@ $config = fopen("./config/config_db.php", "w") or die("Unable to open file!");
 fwrite($config, "<?php\n");
 fwrite($config, "class DB extends DBmysql\n");
 fwrite($config, "{\n");
-fwrite($config, "    public \$dbhost = " . $url['host'] . $url['port'] . ";\n");
+fwrite($config, "    public \$dbhost = " . $url['host'] . ":" . $url['port'] . ";\n");
 fwrite($config, "    public \$dbuser = " . $url['user'] . ";\n");
 fwrite($config, "    public \$dbpassword = " . $url['pass'] . ";\n");
 fwrite($config, "    public \$dbdefault = " . substr($url['path'], 1) . ";\n");
