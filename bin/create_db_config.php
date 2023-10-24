356a1435126c32be3aa099caeb8c57d94af89f17
gabrielv <vallereaugabriel@gmail.com>
2023-10-24 15:23:55 +0200

    :construction_worker: Manually create db config file from environment

---
+++ bin/create_db_config.php
@@ -0,0 +1,18 @@
+<?php
+$url = parse_url(getenv("DATABASE_URL"));
+$config = fopen("../config/config_db.php", "w") or die("Unable to open file!");
+
+fwrite($config, "<?php\n");
+fwrite($config, "class DB extends DBmysql\n");
+fwrite($config, "{\n");
+fwrite($config, "    public \$dbhost = " . $url['host'] . $url['port'] . ";\n");
+fwrite($config, "    public \$dbuser = " . $url['user'] . ";\n");
+fwrite($config, "    public \$dbpassword = " . $url['pass'] . ";\n");
+fwrite($config, "    public \$dbdefault = " . substr($url['path'], 1) . ";\n");
+fwrite($config, "    public \$use_utf8mb4 = true;\n");
+fwrite($config, "    public \$allow_myisam = false;\n");
+fwrite($config, "    public \$allow_datetime = false;\n");
+fwrite($config, "    public \$allow_signed_keys = false;\n");
+fwrite($config, "}\n\n");
+
+fclose($config);
